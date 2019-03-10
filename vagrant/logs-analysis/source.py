4f6416687b2d0a00f6533262ff515bca3031e51b
Rusty Abbott <rusty@hutchinson-island.com>
2019-03-10 12:06:48 -0400

    Create log_view, add queries, print results

--- vagrant/logs-analysis/source.py
+++ vagrant/logs-analysis/source.py
@@ -2,25 +2,27 @@
 
 import psycopg2
 
-# VIEWS
+# Create article_view
 #
 # create view article_view as select author,title,count(*) as views from articles,log where log.path like concat('%',articles.slug) group by articles.title,articles.author order by views desc;
-#
 
 question1 = ("What are the three most popular articles of all time?")
-query1 = (
-    "select title,views from article_view limit 3;"
-)
+query1 = ("select title,views from article_view limit 3;")
 
 question2 = ("Who are the most popular article authors of all time?")
+query2 = ("select authors.name,sum(article_view.views) as views from article_view,authors where authors.id = article_view.author group by authors.name order by views desc;")
+
+# Create log_view
+#
+# create view log_view as select date(time),round(100.0*sum(case log.status when '200 OK' then 0 else 1 end)/count(log.status),2) as "Percent Error" from log group by date(time) order by "Percent Error" desc;
 
 question3 = ("On which days did more than 1% of requests lead to errors?")
+query3 = ("select * from log_view where \"Percent Error\" > 1;")
 
 def connect(database="news"):
     try:
         connection = psycopg2.connect("dbname={}".format(database))
         cursor = connection.cursor()
-        print ("Database connection successful...")
         return connection, cursor
     except (Exception, psycopg2.Error) as error:
         print ("Database connection failed: ", error)
@@ -34,9 +36,19 @@ def results(query):
 
 def print_results(query_results):
     print (query_results[1])
-    for index, results in enumerate(query_results[0]):
+    for index,results in enumerate(query_results[0]):
         print (str(results[0] + " - " + str(results[1]) + " views."))
+    print ("\n")
+
+def print_request_errors(query_results):
+    print (query_results[1])
+    for results in query_results[0]:
+        print (str(str(results[0]) + " - " + str(results[1]) + "% request errors."))
 
 if __name__ == '__main__':
     popular_articles = results(query1), question1
+    popular_authors = results(query2), question2
+    request_errors = results(query3), question3
     print_results(popular_articles)
+    print_results(popular_authors)
+    print_request_errors(request_errors)
